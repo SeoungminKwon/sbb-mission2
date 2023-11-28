cab7051335f1635a7da46b48b111d5c93d05c7ce
SeoungminKwon <tjdalsrnjs10@gmail.com>
2023-11-29 02:00:30 +0900

    3-6 join membership-test

---
+++ src/main/java/com/example/sbbmission2/user/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.sbbmission2.user;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<SiteUser, Long> {
+
+}
