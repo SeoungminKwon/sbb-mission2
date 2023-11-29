31aae9852b19d3f3071c06a59638814fd9d924b1
SeoungminKwon <tjdalsrnjs10@gmail.com>
2023-11-30 00:21:58 +0900

    3-12 Anchor

--- src/main/java/com/example/sbbmission2/answer/AnswerController.java
+++ src/main/java/com/example/sbbmission2/answer/AnswerController.java
@@ -38,9 +38,8 @@ public class AnswerController {
             model.addAttribute("question", question);
             return "question_detail";
         }
-
-        answerService.create(question, answerForm.getContent(), siteUser);
-        return String.format("redirect:/question/detail/%s", id);
+        Answer answer = this.answerService.create(question, answerForm.getContent(), siteUser);
+        return String.format("redirect:/question/detail/%s#answer_%s", answer.getQuestion().getId(), answer.getId());
     }
 
     @PreAuthorize("isAuthenticated()")
@@ -65,7 +64,7 @@ public class AnswerController {
             throw new DataNotFoundException("수정권한이 없습니다.");
         }
         this.answerService.modify(answer, answerForm.getContent());
-        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+        return String.format("redirect:/question/detail/%s#answer_%s", answer.getQuestion().getId(), answer.getId());
     }
 
 
@@ -87,7 +86,7 @@ public class AnswerController {
         Answer answer = answerService.getAnswer(id);
         SiteUser siteUser = this.userService.getUser(principal.getName());
         answerService.vote(answer, siteUser);
-        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+        return String.format("redirect:/question/detail/%s#answer_%s", answer.getQuestion().getId(), answer.getId());
     }
 }
 
